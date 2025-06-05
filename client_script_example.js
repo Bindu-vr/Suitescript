765f99ee236e65bf397947988f165b0911c62035
Bindu V R <binduvr881@gmail.com>
2025-06-05 11:13:24 +0530

    Create client_script_example.js

---
+++ client_script_example.js
@@ -0,0 +1,13 @@
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ */
+define([], function() {
+    function pageInit(context) {
+        alert('Page loaded!');
+    }
+
+    return {
+        pageInit: pageInit
+    };
+});
